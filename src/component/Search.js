367c4ce235df826d38992fe32edcc21579ad3838
andhikabrosnan <andhikabrosnan@gmail.com>
2021-07-15 07:12:04 +0700

    list building

--- src/component/Search.js
+++ src/component/Search.js
@@ -2,12 +2,13 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     // axios request Wikipedia API
     const search = async () => {
-      await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
@@ -16,21 +17,35 @@ const Search = () => {
           srsearch: term,
         },
       });
+      setResults(data.query.search);
     };
 
     search();
   }, [term]);
 
-  return (
-    <div className="ui form">
-      <div className="field">
-        <label>Enter Search Term: </label>
-        <input
-          value={term}
-          onChange={(e) => setTerm(e.target.value)}
-          className="input"
-        />
+  const renderedResults = results.map((results) => {
+    return (
+      <div key={results.pageid} className="item">
+        <div className="content">
+          <div className="header">{results.title}</div>
+          {results.snippet}
+        </div>
       </div>
+    );
+  });
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term: </label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
